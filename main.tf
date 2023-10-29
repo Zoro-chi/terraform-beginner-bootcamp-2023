22a29abc84fe33eb2257abde6e344e14617580fe
Zoro <jagbetuyi001@gmail.com>
2023-10-29 18:30:54 +0100

    #17 Refactor s3 bucket into the new terrahouse_aws module

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "terraform_website_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
+terraform {
+  # cloud {
+  #   organization = "junzy"
+  #   workspaces {
+  #     name = "terra-house-V"
+  #   }
+  # }
 }
 
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -13,6 +13,11 @@ terraform {
   }
 }
 
-provider "aws" {
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "terraform_website_bucket" {
+  bucket = var.bucket_name
 
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
