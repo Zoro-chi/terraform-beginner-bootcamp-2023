de05464cd5b2decfa83117741911003538e5616d
Zoro <jagbetuyi001@gmail.com>
2023-11-02 11:39:25 +0100

    #19 Enabled static website hosting and uploaded error.html and index.html via terraform

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -6,6 +6,7 @@ variable "user_uuid" {
     error_message    = "The user_uuid value is not a valid UUID."
   }
 }
+
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
@@ -16,5 +17,25 @@ variable "bucket_name" {
       can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
     )
     error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
+  }
+}
+
+variable "index_html_filepath" {
+  description = "The file path for index.html"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exist."
+  }
+}
+
+variable "error_html_filepath" {
+  description = "The file path for index.html"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The provided path for error.html does not exist."
   }
 }
