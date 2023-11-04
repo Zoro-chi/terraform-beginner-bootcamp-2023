e35fcbb772668542e464b17294123e999685cae1
Zoro-chi <jagbetuyi001@gmail.com>
2023-11-04 12:17:47 +0100

    #25 Modified cdn invalidations to run on content_version change. (#26)

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -62,3 +62,18 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cloudfront_default_certificate = true
   }
 }
+
+#! THIS ONLY ALLOWS CDN INVALIDATION ON CONTENT_VERSION CHANGE
+resource "terraform_data" "invalidate_cache" {
+  triggers_replace = terraform_data.content_version.output
+
+  provisioner "local-exec" {
+    # https://developer.hashicorp.com/terraform/language/expressions/strings#heredoc-strings
+    command = <<COMMAND
+aws cloudfront create-invalidation \
+--distribution-id ${aws_cloudfront_distribution.s3_distribution.id} \
+--paths '/*'
+    COMMAND
+
+  }
+}
